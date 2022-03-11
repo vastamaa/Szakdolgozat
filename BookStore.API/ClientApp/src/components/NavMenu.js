76eafce263611bcfef48954da9931ec1a12ab770
almasim <myl4n1@gmail.com>
2022-03-11 11:49:26 +0100

    Sosehinnédel hogy csinálta meg a burgermenüt
    burgir

--- BookStore.API/ClientApp/src/components/NavMenu.js
+++ BookStore.API/ClientApp/src/components/NavMenu.js
@@ -7,6 +7,7 @@ import { readCookie } from "./CookieHandler";
 import { logOut } from "./Log";
 import './NavMenu.css';
 import { FaBars } from "@react-icons/all-files/fa/FaBars";
+import { AiOutlineLeft } from "@react-icons/all-files/ai/AiOutlineLeft";
 
 
 export class NavMenu extends Component {
@@ -14,60 +15,79 @@ export class NavMenu extends Component {
 
     constructor(props) {
         super(props);
-
-        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.toggleBurgir=this.toggleBurgir.bind(this);
         this.state = {
-            collapsed: true,
+            collapsed: false,
             loggedIn: readCookie("token"),
-            userName: ""
         };
+
     }
+    toggleBurgir() {
+       const currentState=this.state.collapsed;
+       this.setState({collapsed: !currentState});
+     }
+
 
     componentDidMount() {
         this.getUserName();
     }
 
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
-
     render() {
         return (
             <header className='stick-the-navbar'>
-                {/* Example -- 'null = falshy' {this.state.loggedIn ? console.log("be van jelentkezve") : console.log("nincs bejelentkezve") };*/}
-
-                <div className='NavContainer NavWrapper'>
-                    <FaBars className='bars'></FaBars>
-                    <div className='NavLeft'>
-                        <span className='Lang'>EN</span>
-                        <div className='SearchContainer'>
-                            <input className='NavInput'></input>
-                            <IoSearchSharp className='SearchButton' />
-                        </div>
+            {/* Example -- 'null = falshy' {this.state.loggedIn ? console.log("be van jelentkezve") : console.log("nincs bejelentkezve") };*/}
+    
+            <div className='NavContainer NavWrapper'>
+                <FaBars className='bars' onClick={this.toggleBurgir}></FaBars>
+                <div className='NavLeft'>
+                    <span className='Lang'>EN</span>
+                    <div className='SearchContainer'>
+                        <input className='NavInput'></input>
+                        <IoSearchSharp className='SearchButton' />
                     </div>
-
-                    <div className='NavCenter'>
-                        <NavLink tag={Link} to="/"> <h1 className='NavLogo text-dark'>Litera</h1></NavLink>
-                    </div>
-                    {this.state.loggedIn ? <div className='NavRight'>
-                        <NavLink tag={Link} className='NavMenuItem text-dark' to="/books" >Books</NavLink>
-                        <AiOutlineShoppingCart size="30px "></AiOutlineShoppingCart>
-                        {/*<NavLink tag={Link} className='NavMenuItem text-dark' to="/account/login" >Welcome, {this.state.userName}</NavLink>*/}
-                        <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/register" >Register</NavLink>
-                        <div className="dropdown">
-                            <span className="dropdown-toggle NavMenuItem text-dark" data-bs-toggle="dropdown">Welcome, {this.state.userName}</span>
-                            <div class="dropdown-menu">
-                                {/*                                <a href="#" className="dropdown-item">Action</a>*/}
-                                <NavLink tag={Link} onClick={logOut} className="dropdown-item NavMenuItem text-dark" to="/">Logout</NavLink>
-                            </div>
-                        </div>
-                    </div> : <div className='NavRight'>
-                        <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/login" >Login</NavLink>
-                        <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/register" >Register</NavLink>
-                    </div>}
                 </div>
+    
+                <div className='NavCenter'>
+                    <NavLink tag={Link} to="/"> <h1 className='NavLogo text-dark'>Litera</h1></NavLink>
+                </div>
+                {this.state.loggedIn ? <div className='NavRight'>
+                    <NavLink tag={Link} className='NavMenuItem text-dark' to="/books" >Books</NavLink>
+                    <AiOutlineShoppingCart size="30px "></AiOutlineShoppingCart>
+                    <div className="dropdown">
+                        <span className="dropdown-toggle NavMenuItem text-dark" data-bs-toggle="dropdown">Welcome, {this.state.userName}</span>
+                        <div class="dropdown-menu">
+                                {/*<a href="#" className="dropdown-item">Action</a>*/}
+                            <NavLink tag={Link} onClick={logOut} className="dropdown-item NavMenuItem text-dark" to="/">Logout</NavLink>
+                        </div>
+                    </div>
+                </div> : <div className='NavRight'>
+                    <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/login" >Login</NavLink>
+                    <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/register" >Register</NavLink>
+                </div>}
+            </div>
+            <div className='burgirMenu'  style={{transform: this.state.collapsed ? 'translateX(0)':'translateX(100%)'}} > 
+            <div className='burgerClose' onClick={this.toggleBurgir}>               
+            <AiOutlineLeft  ></AiOutlineLeft>
+            </div>
+            {this.state.loggedIn ? <div className='burgirDiv'>
+                    <NavLink tag={Link} className='NavMenuItem text-dark burgirHead' to="/">Home</NavLink>
+                    <div className='SearchContainerBurgir burgirList '>
+                        <input className='NavInputBurgir'></input>
+                        <IoSearchSharp className='SearchButtonBurgir' />
+                    </div>
+                    <NavLink tag={Link} className='NavMenuItem text-dark ' to="/books" >Books</NavLink>
+                    <NavLink tag={Link} onClick={logOut} className='NavMenuItem text-dark' to="/">Logout</NavLink>
+                </div> : <div className='burgirDiv'>
+                    <NavLink tag={Link} className='NavMenuItem text-dark burgirHead' to="/">Home</NavLink>
+                    <div className='SearchContainerBurgir burgirList'>
+                        <input className='NavInputBurgir'></input>
+                        <IoSearchSharp className='SearchButtonBurgir' />
+                    </div>
+                    <NavLink tag={Link} className='NavMenuItem text-dark burgirList' to="/account/login" >Login</NavLink>
+                    <NavLink tag={Link} className='NavMenuItem text-dark burgirList' to="/account/register" >Register</NavLink>
+ 
+                </div>}
+            </div>
             </header>
         );
     }
@@ -85,64 +105,3 @@ export class NavMenu extends Component {
         }
     }
 }
-
-
-// export class NavMenu extends Component {
-//     static displayName = NavMenu.name;
-
-//     constructor(props) {
-//         super(props);
-//         this.toggleBurgir=this.toggleBurgir.bind(this);
-//         this.state = {
-//             collapsed: false,
-//             loggedIn: readCookie("token"),
-//         };
-
-//     }
-//     toggleBurgir() {
-//        const currentState=this.state.collapsed;
-//        this.setState({collapsed: !currentState});
-//      }
-
-
-//     componentDidMount() {
-//         this.getUserName();
-//     }
-
-
-// render() {
-//     return (
-//         <header className='stick-the-navbar'>
-//         {/* Example -- 'null = falshy' {this.state.loggedIn ? console.log("be van jelentkezve") : console.log("nincs bejelentkezve") };*/}
-
-//         <div className='NavContainer NavWrapper'>
-//             <FaBars className='bars' onClick={this.toggleBurgir}></FaBars>
-//             <div className='NavLeft'>
-//                 <span className='Lang'>EN</span>
-//                 <div className='SearchContainer'>
-//                     <input className='NavInput'></input>
-//                     <IoSearchSharp className='SearchButton' />
-//                 </div>
-//             </div>
-
-//             <div className='NavCenter'>
-//                 <NavLink tag={Link} to="/"> <h1 className='NavLogo text-dark'>Litera</h1></NavLink>
-//             </div>
-//             {this.state.loggedIn ? <div className='NavRight'>
-//                 <NavLink tag={Link} className='NavMenuItem text-dark' to="/books" >Books</NavLink>
-//                 <AiOutlineShoppingCart size="30px "></AiOutlineShoppingCart>
-//                 <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/login" >Welcome, {this.state.userName}</NavLink>
-//                 <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/register" >Register</NavLink>
-//                 <NavLink tag={Link} onClick={logOut} className='NavMenuItem text-dark' to="/">Logout</NavLink>
-//             </div> : <div className='NavRight'>
-//                 <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/login" >Login</NavLink>
-//                 <NavLink tag={Link} className='NavMenuItem text-dark' to="/account/register" >Register</NavLink>
-//             </div>}
-//         </div>
-//         <div className='burgirMenu'  style={{transform: this.state.collapsed ? 'translateX(0)':'translateX(100%)'}} onClick={this.setState({collapsed:true})}>
-//         <AiOutlineLeft onClick={this.toggleBurgir}></AiOutlineLeft>
-//         </div>
-//         </header>
-
-//     );
-// }
